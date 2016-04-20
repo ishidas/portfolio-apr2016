01d5bd30f6f52a0c41a76965a4623e6ae8b5a033
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-20 11:41:33 -0700

    added navbar template fixed angular bug angular bug: file name was app-module and app-directives.js and didn't work, but changed to app and directives.js and works

---
+++ cli-backend/app/app.js
@@ -0,0 +1,4 @@
+(function(){
+  var angular = require('angular');
+  angular.module('myApp', []);
+})();
---
+++ cli-backend/app/directives.js
@@ -0,0 +1,12 @@
+(function(){
+  var angular = require('angular');
+  var app = angular.module('myApp');
+
+  app.directive('myNavBar', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/navbar.html'
+    };
+  });
+  
+})();
