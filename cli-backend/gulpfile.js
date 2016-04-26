f0fe26e3c9791fcc09b120c05af052d0e4000d06
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-26 16:50:37 -0700

    added a background img to a folder, added styling using sass

--- cli-backend/gulpfile.js
+++ cli-backend/gulpfile.js
@@ -54,7 +54,10 @@ gulp.task('bundle:dev', function(){
     },
     module: {
       loaders: [
-        {test:  /\.scss$/, loaders: ['style', 'css', 'sass']}
+        {test:  /\.scss$/, loaders: ['style', 'css', 'sass']},
+        {test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]}
       ]
     }
   }))
