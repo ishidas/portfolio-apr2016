064f0e5a64d69cebaca244ae0e4f8a132d2f330b
Sawako Ishida <sawako.yoriko@gmail.com>
2016-05-09 13:25:25 -0700

    added crud to my blog server

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,13 @@ app.get('/blog/articles', (req, res)=>{
   });
 });
 
+app.get('/blog/articles/:id', (req, res)=>{
+  models.Blog.findOne({_id: req.params.id},(err, blog)=>{
+    if(err) return res.json({error: err});
+    res.json(blog);
+  });
+});
+
 app.post('/admin/blog', (req, res)=>{
   var newBlog = new models.Blog(req.body);
   newBlog.save((err)=>{
@@ -28,6 +35,18 @@ app.post('/admin/blog', (req, res)=>{
   });
 });
 
+app.put('/admin/blog/article/:id', (req, res)=>{
+  models.Blog.findOneAndUpdate({_id: req.params.id},{$set: req.body}, {new: true},(err, updatedBlog)=>{
+    if(err) return res.json({error: err});
+    res.json(updatedBlog);
+  });
+});
 
+app.delete('/admin/blog/article/:id', (req, res)=>{
+  models.Blog.findOneAndRemove({_id: req.params.id}, (err)=>{
+    if(err) return res.json({error: err});
+    res.json({msg: 'success!'});
+  });
+});
 
 app.listen(3000, ()=> console.log('Backend server listening at 3000'));
